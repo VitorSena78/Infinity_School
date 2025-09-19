4f46f40e7668b8f5c2ed86174959d4552fdbcfe6
Vitor <vitorsena78@gmail.com>
2025-09-19 08:25:09 -0300

    calculadora de área

---
+++ prova1.py
@@ -0,0 +1,8 @@
+print("---- Programa para cálculo de área de retângulos ----")
+
+a = float(input("digite a altura do retangulo: "))
+b = float(input("digite a base do retangulo: "))
+
+area = b * a
+
+print(f"a área do retângulo é: {area}")
