826a2b51d6b58562a19a086153a525128e7fed40
Vitor <vitorsena78@gmail.com>
2025-11-18 15:02:24 -0300

    comentario e testes simples

--- JV_aula4/aula04.py
+++ JV_aula4/aula04.py
@@ -23,6 +23,12 @@
 # for i in range(10, 0, -2):
 #     print(i)
 
+# range(inicio(10), parada(0), stape(-2))
+#
+# inicio: Onde a sequência numérica começa. Neste caso, 10.
+# parada: Onde a sequência numérica termina (exclusive). A sequência vai até o valor antes da parada, neste caso, 1.
+# stape: O valor que é adicionado (ou subtraído) a cada passo. Neste caso, -2, significando que a contagem é decrescente.
+
 nome = "Antonio"
 for letra in nome:
     print(letra)
---
+++ teste.py
@@ -0,0 +1,6 @@
+print(list(range(10,1,-1)))
+
+lista =[1,2,3]
+for c in range(len(lista)):
+    lista[c]+=3
+print(lista)
