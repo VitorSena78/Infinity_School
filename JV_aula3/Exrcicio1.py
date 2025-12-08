4daf7e5d0d7f3772ca61d76e493e862af5bd637d
Vitor <vitorsena78@gmail.com>
2025-12-08 13:03:20 -0300

    Exercicios aula 3

---
+++ JV_aula3/Exrcicio1.py
@@ -0,0 +1,10 @@
+nota=float(input("digite uma nota entre 0 e 10: "))
+
+if nota>=9:
+    print("Exelente")
+elif nota>=7:
+    print("Boa")
+elif nota>=5:
+    print("Regular")
+else:
+    print("Reprovado")
